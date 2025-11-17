7a76d1756e47d4a09dc5aa5949303b8d31b12df3
mhassini <mhassini@gmail.com>
2025-11-17 12:15:46 +0100

    config nexus

--- src/main/java/tn/m104/rh/control/StudentController.java
+++ src/main/java/tn/m104/rh/control/StudentController.java
@@ -2,10 +2,17 @@ package tn.m104.rh.control;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import tn.m104.rh.entity.Student;
 import tn.m104.rh.service.IStudentService;
 
--- src/main/java/tn/m104/rh/repository/StudentRepository.java
+++ src/main/java/tn/m104/rh/repository/StudentRepository.java
@@ -1,8 +1,8 @@
 package tn.m104.rh.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import tn.m104.rh.entity.Student;
 
 @Repository
